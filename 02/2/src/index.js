e7003f452fdd7e57cb02983bb9879867b1a9da61
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-07 21:27:21 +0300

    2.2

---
+++ 02/2/src/index.js
@@ -0,0 +1,6 @@
+const result = document.querySelector(".counter__result");
+const decrement = document.querySelector("[data-action='decrement']");
+const reset = document.querySelector("[data-action='reset']");
+const increment = document.querySelector("[data-action='increment']");
+
+console.log(decrement);
