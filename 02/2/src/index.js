8d29ef9f4f3bfc8150aa9273a9b25f39004c9626
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-07 23:53:02 +0300

    2.2

--- 02/2/src/index.js
+++ 02/2/src/index.js
@@ -1,16 +1,34 @@
+import "./index.css";
 import { store, increment, decrement, reset } from "./store.js";
-import './index.css'
+
 const resultElem = document.querySelector(".counter__result");
 const decrementBtn = document.querySelector("[data-action='decrement']");
 const resetBtn = document.querySelector("[data-action='reset']");
 const incrementBtn = document.querySelector("[data-action='increment']");
 
 const onIncrement = () => {
-  store.despatch(increment());
+  store.dispatch(increment());
+};
+
+const onDecrement = () => {
+  store.dispatch(decrement());
+};
+
+const onReset = () => {
+  store.dispatch(reset());
 };
 
 incrementBtn.addEventListener("click", onIncrement);
+decrementBtn.addEventListener("click", onDecrement);
+resetBtn.addEventListener("click", onReset);
 
 store.subscribe(() => {
-  console.log(store.getSate());
+  console.log(store.getState());
+  const state = store.getState();
+  const currentValue = state.value;
+  const historyString = state.history.join(" ");
+
+  resultElem.textContent = state.value === 0
+    ? ""
+    : `${historyString} = ${currentValue}`;
 });
