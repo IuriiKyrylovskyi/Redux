0b3dba412ad83833bdf1b4621356354a8e148cd6
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-07 20:20:32 +0300

    2.1

--- 02/counter/src/index.js
+++ 02/counter/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const INCREMENT = "CREATE/INCREMENT";
+const DECREMENT = "CREATE/DECREMENT";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decremrnt = () => {
+  return {
+    type: DECREMENT,
+  }
+}
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type){
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+// console.log(store);
