035a94435f2b783a42f1c8c7beabbe8c579371c3
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-07 22:01:00 +0300

    store

--- 02/2/src/index.js
+++ 02/2/src/index.js
@@ -1,6 +1,6 @@
-const result = document.querySelector(".counter__result");
-const decrement = document.querySelector("[data-action='decrement']");
-const reset = document.querySelector("[data-action='reset']");
-const increment = document.querySelector("[data-action='increment']");
+const resultElem = document.querySelector(".counter__result");
+const decrementBtn = document.querySelector("[data-action='decrement']");
+const resetBtn = document.querySelector("[data-action='reset']");
+const incrementBtn = document.querySelector("[data-action='increment']");
 
 console.log(decrement);
---
+++ 02/2/src/store.js
@@ -0,0 +1,25 @@
+import { createStore } from "redux";
+
+const INCREMENT = "COUNTER/INCREMENT";
+const DECREMENT = "COUNTER/DECREMENT";
+const RESET = "COUNTER/RESET";
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+export const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+
+export const store = createStore(counterReducer);
