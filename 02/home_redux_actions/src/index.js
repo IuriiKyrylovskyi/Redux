0dbc70e5a7b8365534e8db54070cc2d821c61873
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-08 16:09:06 +0300

    2.home

--- 02/home_redux_actions/src/index.js
+++ 02/home_redux_actions/src/index.js
@@ -1,30 +1,6 @@
-import store, { increment, decrement, reset } from "./store.js";
+import store from "./store.js";
+import { addUser, deleteUser } from "./users.actions";
 
-const resultElem = document.querySelector(".counter__result");
-const decrementBtn = document.querySelector("[data-action='decrement']");
-const resetBtn = document.querySelector("[data-action='reset']");
-const incrementBtn = document.querySelector("[data-action='increment']");
+store.dispatch(addUser(user));
 
-const onIncrement = () => {
-  store.dispatch(increment());
-};
-
-const onDecrement = () => {
-  store.dispatch(decrement());
-};
-
-const onReset = () => {
-  store.dispatch(reset());
-};
-
-incrementBtn.addEventListener("click", onIncrement);
-decrementBtn.addEventListener("click", onDecrement);
-resetBtn.addEventListener("click", onReset);
-
-store.subscribe(() => {
-  const state = store.getState();
-  const currentValue = state.history.reduce((acc, value) => acc + parseInt(value), 0);
-  const historyString = state.history.join("");
-
-  resultElem.textContent = currentValue === 0 ? "" : `${historyString} = ${currentValue}`;
-});
+store.dispatch(deleteUser(id));
--- 02/home_redux_actions/src/store.js
+++ 02/home_redux_actions/src/store.js
@@ -1,5 +1,6 @@
 import { createStore } from "redux";
 import { usersReducer } from "./users.reducer.js";
-import { addUser, deleteUser } from "./users.actions";
 
 const store = createStore(usersReducer);
+
+export default store;
