1133754d9058c98de28e868dc03759ed99dd05ed
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-08 16:47:52 +0300

    2.h

--- 02/home_redux_actions/src/users.reducer.js
+++ 02/home_redux_actions/src/users.reducer.js
@@ -5,11 +5,15 @@ const initState = {
 export const usersReducer = (state = initState, action) => {
   switch (action.type) {
     case ADD_USER:
-      return state.usersList.concat(user);
-    
+      return {
+        usersList: state.usersList.concat(user),
+      };
+
     case REMOVE_USER:
-      return state.usersList.filter((user) => user.id !== id);
-   
+      return {
+        usersList: state.usersList.filter((user) => user.id !== id),
+      };
+
     default:
       return state;
   }
