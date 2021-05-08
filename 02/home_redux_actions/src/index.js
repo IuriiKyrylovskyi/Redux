21ceb8d29b48f875614f4d988eb86873cdc76190
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-08 17:39:29 +0300

    2.h

--- 02/home_redux_actions/src/index.js
+++ 02/home_redux_actions/src/index.js
@@ -1,13 +1,13 @@
 import store from "./store.js";
 import { addUser, deleteUser } from "./users.actions";
 
-const newUser = { id: 76, name: "Sarah" };
+// const newUser = { id: 76, name: "Sarah" };
 
-store.dispatch(addUser(newUser));
+store.dispatch(addUser());
 
-const userIdToDelete = 76;
+// const userIdToDelete = 76;
 
-store.dispatch(deleteUser(userIdToDelete));
+store.dispatch(deleteUser());
 
 store.subscribe(() => {
   store.getState();
