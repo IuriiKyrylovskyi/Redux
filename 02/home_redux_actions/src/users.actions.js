670686feec09abb40408ddc262d25e181a5a4e0d
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-08 18:29:55 +0300

    h

--- 02/home_redux_actions/src/users.actions.js
+++ 02/home_redux_actions/src/users.actions.js
@@ -1,16 +1,18 @@
 const ADD_USER = "USERS/ADD_USER";
 const DELETE_USER = "USERS/DELETE_USER";
 
-export const addUser = () => {
+export const addUser = (user) => {
   return {
     type: ADD_USER,
-    payload: { userId, userName },
+    payload: user,
+    // payload: { userId, userName },
   };
 };
 
-export const deleteUser = () => {
+export const deleteUser = (userId) => {
   return {
     type: DELETE_USER,
     payload: userId,
+    // payload: userId,
   };
 };
