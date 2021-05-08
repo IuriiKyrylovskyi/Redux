1a5caeb90b709601a128e2216c07bd1a43743a57
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-08 16:50:32 +0300

    payload

--- 02/home_redux_actions/src/users.reducer.js
+++ 02/home_redux_actions/src/users.reducer.js
@@ -6,12 +6,12 @@ export const usersReducer = (state = initState, action) => {
   switch (action.type) {
     case ADD_USER:
       return {
-        usersList: state.usersList.concat(user),
+        usersList: state.usersList.concat(action.payload),
       };
 
     case REMOVE_USER:
       return {
-        usersList: state.usersList.filter((user) => user.id !== id),
+        usersList: state.usersList.filter((user) => user.id !== action.payload),
       };
 
     default:
