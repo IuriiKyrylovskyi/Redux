f34af11686b3ee7c50fb3fee0c15dd1f0849bc57
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-11 23:56:02 +0300

    h

--- 02/home_redux_actions/src/index.js
+++ 02/home_redux_actions/src/index.js
@@ -1,18 +1,16 @@
 import store from "./store.js";
 import { addUser, deleteUser } from "./users.actions.js";
 
-// const newUser = { id: 76, name: "Sarah" };
+const newUser = { id: '76', name: "Sarah" };
 
 store.dispatch(addUser(newUser));
 store.subscribe(() => {
   store.getState();
-  // console.log(store.getState());
 });
-// const userIdToDelete = 76;
+const userIdToDelete = '76';
 
 store.dispatch(deleteUser(userIdToDelete));
 
 store.subscribe(() => {
   store.getState();
-  // console.log(store.getState());
 });
