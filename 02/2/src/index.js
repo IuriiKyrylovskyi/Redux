18471441cd809f8c8322feaccca6bd92519f4ea5
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-11 20:36:11 +0300

    2.2

--- 02/2/src/index.js
+++ 02/2/src/index.js
@@ -22,6 +22,12 @@ incrementBtn.addEventListener("click", onIncrement);
 decrementBtn.addEventListener("click", onDecrement);
 resetBtn.addEventListener("click", onReset);
 
+// const toAddSign = (e) => {
+//   console.log(e.target.dataset.action);
+//   e.target.dataset.action === 'increment' ? "+" : "";
+// }
+
+
 store.subscribe(() => {
   const state = store.getState();
   const currentValue = state.history.reduce((acc, value) => acc + parseInt(value), 0);
