c8d7937e6ae660aaa1b7977ea0002de51f87c7a1
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-14 00:08:19 +0300

    dispatch

--- lesson4/task1/src/Counter.jsx
+++ lesson4/task1/src/Counter.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
+import { connect } from "react-redux";
+import * as counterActions from "./counter.actions";
 
-function Counter() {
-  const [counter, setCounter] = useState(0);
-
+function Counter({ counter, increment, decrement, reset }) {
   return (
     <div className="counter">
-      <button className="counter__button" onClick={() => setCounter(counter - 1)}>
+      <button className="counter__button" onClick={decrement}>
         -
       </button>
-      <span className="counter__value" onClick={() => setCounter(0)}>
+      <span className="counter__value" onClick={reset}>
         {counter}
       </span>
-      <button className="counter__button" onClick={() => setCounter(counter + 1)}>
+      <button className="counter__button" onClick={increment}>
         +
       </button>
     </div>
   );
 }
 
-export default Counter;
+const mapState = (state) => {
+  return {
+    counter: state,
+  };
+};
+
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+export default connector(Counter);
