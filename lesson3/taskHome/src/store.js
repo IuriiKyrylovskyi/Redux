ba6be9306058d7df8929d1c7d6b1bd85cf2b19be
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-13 15:54:10 +0300

    combineReducers

--- lesson3/taskHome/src/store.js
+++ lesson3/taskHome/src/store.js
@@ -1,12 +1,12 @@
-import { createStore, combineReducer } from "redux";
+import { createStore, combineReducers } from "redux";
 import languageReducer from "./language.reducer";
 import cartReducer from "./cart.reducer";
 import userReducer from "./user.reducer";
 
-const appStore = combineReducer({
-  languageReducer,
-  cartReducer,
-  userReducer,
+const appStore = combineReducers({
+  language: languageReducer,
+  cart: cartReducer,
+  user: userReducer,
 });
 
 const store = createStore(appStore, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
