b646c61b68ce8dedf9ff5fa4e70b3f75ebc0f5ad
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-14 14:57:06 +0300

    user

--- lesson4/task2/src/users/Users.jsx
+++ lesson4/task2/src/users/Users.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import * as usersActions from "./users.actions";
 
-function Users() {
+function Users({ usersList, addUser, deleteUser }) {
   return (
     <div className="users">
-      <button className="users__create-btn">Create user</button>
+      <button className="users__create-btn" onClick={addUser}>
+        Create user
+      </button>
       <ul className="users__list">
-        <li className="users__list-item">
-          <span>User # 232286</span>
-          <button className="users__delete-btn">+</button>
-        </li>
-        <li className="users__list-item">
-          <span>User # 551949</span>
-          <button className="users__delete-btn">+</button>
-        </li>
-        <li className="users__list-item">
-          <span>User # 328468</span>
-          <button className="users__delete-btn">+</button>
-        </li>
+        {usersList.map((user) => (
+          <li key={user.userId} className="users__list-item">
+            <span>User # {user.userId}</span>
+            <button className="users__delete-btn" onClick={deleteUser}>
+              +
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
 
-export default Users
+const mapState = (state) => {
+  return {
+    users: {
+      usersList: state,
+    },
+  };
+};
+
+const mapDispatch = {
+  addUser: usersActions.addUser,
+  deleteUser: usersActions.deleteUser,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+export default connector(Users);
