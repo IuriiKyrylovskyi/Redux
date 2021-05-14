d4d905f1eb4036a67b1e15b0c0f617b28b4e2de5
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-14 20:45:23 +0300

    ITEMS_PER_PAGE

--- lesson4/task_home/src/users/Pagination.jsx
+++ lesson4/task_home/src/users/Pagination.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
 function Pagination({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) {
-  let isPrevPageAvailable = currentPage === 0;
-  let isNextPageAvailable = currentPage === Math.round(totalItems / itemsPerPage);
+  let isPrevPageAvailable = currentPage !== 0;
+  // let isNextPageAvailable = totalItems % itemsPerPage === 0 ? currentPage + 1 !== parseInt(totalItems / itemsPerPage) : currentPage !== parseInt(totalItems / itemsPerPage);
+  let isNextPageAvailable = currentPage + 1 !== Math.ceil(totalItems / itemsPerPage);
+  console.log("currentPage", currentPage + 1);
+  // console.log("totalItems", totalItems);
+  // console.log("itemsPerPage", itemsPerPage);
+  // console.log("isNextPageAvailable", isNextPageAvailable);
+  console.log(parseInt(totalItems / itemsPerPage));
 
   return (
     <div className="pagination">
-      <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
-        {!isPrevPageAvailable && "←"}
+      <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable}>
+        {isPrevPageAvailable && "←"}
       </button>
       <span className="pagination__page">{currentPage + 1}</span>
-      <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
-        {!isNextPageAvailable && "→"}
+      <button className="btn" onClick={goNext} disabled={!isNextPageAvailable}>
+        {isNextPageAvailable && "→"}
       </button>
     </div>
   );
--- lesson4/task_home/src/users/UsersList.jsx
+++ lesson4/task_home/src/users/UsersList.jsx
@@ -5,17 +5,26 @@ import * as paginationsActions from "./../pagination.actions";
 import User from "./User";
 import Pagination from "./Pagination";
 
+const ITEMS_PER_PAGE = 3;
+
 function UsersList({ users, goPrev, goNext, currentPage }) {
   const totalItems = users.length;
-  console.log(users);
-  console.log(currentPage);
 
-  const itemsPerPage = 5;
+  const itemsPerPage = ITEMS_PER_PAGE;
+
+  const indexMin = currentPage * itemsPerPage;
+  const indexMax = indexMin + itemsPerPage;
 
   return (
     <>
       <Pagination goPrev={goPrev} goNext={goNext} currentPage={currentPage} totalItems={totalItems} itemsPerPage={itemsPerPage} />
-      <ul className="users">{users.length > 0 && users.map((user) => <User key={user.id} name={user.name} age={user.age} />)}</ul>
+      <ul className="users">{users.length > 0 && users.filter((user, index) => index >= indexMin && index < indexMax).map((user) => <User key={user.id} name={user.name} age={user.age} />)}</ul>
+      {/* <ul className="users">
+        {users.length > 0 &&
+          users.reduce((acc, user, index) => (index >= indexMin && index < indexMax)
+            ? <User key={user.id} name={user.name} age={user.age} />
+            : acc, [])}
+      </ul> */}
     </>
   );
 }
