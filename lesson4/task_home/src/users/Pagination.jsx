f83cdaf9bf281393b8d2a96e7e96082346cccf83
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-14 19:35:19 +0300

    disabled

--- lesson4/task_home/src/users/Pagination.jsx
+++ lesson4/task_home/src/users/Pagination.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 function Pagination({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) {
   let isPrevPageAvailable = currentPage === 0;
-  let isNextPageAvailable = currentPage === totalItems;
-
-
+  let isNextPageAvailable = currentPage === Math.round(totalItems / itemsPerPage);
 
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
         ←
       </button>
-      <span className="pagination__page">{currentPage}</span>
+      <span className="pagination__page">{currentPage + 1}</span>
       <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
         →
       </button>
