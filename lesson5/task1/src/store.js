1838799ef07b2de9270d7706ce2ed5dcda4e97b7
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-18 15:29:34 +0300

    store

---
+++ lesson5/task1/src/store.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from "react-redux";
+import usersReducer from "./users/users.reducer";
+
+const appStore = combineReducers({
+  users: usersReducer,
+});
+
+const store = createStore(appStore, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
