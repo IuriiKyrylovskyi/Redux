c97a37fb17d7cb1a82e2bb55e73da6c0e8b3eab3
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-18 14:00:44 +0300

    4h

--- lesson4/task_home/src/users.js
+++ lesson4/task_home/src/users.js
@@ -44,16 +44,16 @@ const users = [
     age: 45,
     name: "Ron",
   },
-  // {
-  //   id: "id-9",
-  //   age: 45,
-  //   name: "Harry",
-  // },
-  // {
-  //   id: "id-10",
-  //   age: 45,
-  //   name: "Anna",
-  // },
+  {
+    id: "id-9",
+    age: 45,
+    name: "Harry",
+  },
+  {
+    id: "id-10",
+    age: 45,
+    name: "Anna",
+  },
 ];
 
 export default users;
