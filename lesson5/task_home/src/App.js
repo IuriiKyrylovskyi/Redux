a3218328e98a9e06e3cb1719b0cbca7c9dc3fdd1
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-18 23:10:57 +0300

    UsersList

--- lesson5/task_home/src/App.js
+++ lesson5/task_home/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import UsersList from "./users/UsersList";
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <Provider store={store}>
+      <UsersList />
+    </Provider>
   );
 }
 
---
+++ lesson5/task_home/src/users/UsersList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import User from "./User";
+
+const UsersList = ({ filtredUsersList }) => {
+  return (
+    <ul class="users">
+      {filtredUsersList.map((user) => {
+        const { id, name, age } = user;
+        return <User key={id} name={name} age={age} />;
+      })}
+    </ul>
+  );
+};
+
+export default UsersList;
