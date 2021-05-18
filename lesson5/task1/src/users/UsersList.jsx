fd328b08ccb536ce88b654857a09d0bf30fa652c
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-18 15:51:52 +0300

    users.actions

--- lesson5/task1/src/users/UsersList.jsx
+++ lesson5/task1/src/users/UsersList.jsx
@@ -1,28 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-import * as usersActions from "./users.actions";
+import { goPrevPage , goNextPage} from "./users.actions";
 import User from "./User";
 import Pagination from "./Pagination";
 
-// import { users } from "../options/users";
-
 const ITEMS_PER_PAGE = 4;
-
-// const currentPage = 0;
 
 const UsersList = ({ users, currentPage, goPrev, goNext }) => {
   const start = currentPage * ITEMS_PER_PAGE;
   const usersToDisplay = users.slice(start, start + ITEMS_PER_PAGE);
   const totalItems = users.length;
-
-  // const goPrev = () => {
-  //   return currentPage !== 0 && start - ITEMS_PER_PAGE && console.log("goPrev");
-  //   // console.log("goPrev");
-  // };
-
-  // const goNext = () => {
-  //   return currentPage !== totalItems && start + ITEMS_PER_PAGE && console.log("goNext");
-  // };
 
   return (
     <>
@@ -53,8 +40,8 @@ const mapState = (state) => {
 };
 
 const mapDispatch = {
-  goPrev: usersActions.goPrevPage,
-  goNext: usersActions.goNextPage,
+  goPrev: goPrevPage,
+  goNext: goNextPage,
 };
 
 const connector = connect(mapState, mapDispatch);
