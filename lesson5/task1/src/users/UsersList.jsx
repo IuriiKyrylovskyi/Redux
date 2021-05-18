b65be890cc3bf02065653a885e1aff4d0889a260
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-18 15:44:01 +0300

    mapState

--- lesson5/task1/src/users/UsersList.jsx
+++ lesson5/task1/src/users/UsersList.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as usersActions from "./users.actions";
 import User from "./User";
 import Pagination from "./Pagination";
 
-import { users } from "../options/users";
+// import { users } from "../options/users";
 
 const ITEMS_PER_PAGE = 4;
 
-const currentPage = 0;
+// const currentPage = 0;
 
-const UsersList = () => {
-  //({ users }) => {
+const UsersList = ({ users, currentPage, goPrev, goNext }) => {
   const start = currentPage * ITEMS_PER_PAGE;
   const usersToDisplay = users.slice(start, start + ITEMS_PER_PAGE);
   const totalItems = users.length;
 
-  const goPrev = () => {
-    return currentPage !== 0 && start - ITEMS_PER_PAGE && console.log("goPrev");
-    // console.log("goPrev");
-  };
+  // const goPrev = () => {
+  //   return currentPage !== 0 && start - ITEMS_PER_PAGE && console.log("goPrev");
+  //   // console.log("goPrev");
+  // };
 
-  const goNext = () => {
-    return currentPage !== totalItems && start + ITEMS_PER_PAGE && console.log("goNext");
-  };
+  // const goNext = () => {
+  //   return currentPage !== totalItems && start + ITEMS_PER_PAGE && console.log("goNext");
+  // };
 
   return (
     <>
@@ -44,4 +45,18 @@ const UsersList = () => {
   );
 };
 
-export default UsersList;
+const mapState = (state) => {
+  return {
+    users: state.users.usersList,
+    currentPage: state.users.currentPage,
+  };
+};
+
+const mapDispatch = {
+  goPrev: usersActions.goPrevPage,
+  goNext: usersActions.goNextPage,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+export default connector(UsersList);
