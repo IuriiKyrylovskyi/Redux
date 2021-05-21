56eb4a88ca6e19b5dc45263befa67261b2970bb7
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-21 16:19:25 +0300

    start

---
+++ lesson6/task1/src/counter.reducer.js
@@ -0,0 +1,20 @@
+import { DECREMENT, INCREMENT, RESET } from "./counter.actions";
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case DECREMENT: {
+      return state - 1;
+    }
+    case INCREMENT: {
+      return state + 1;
+    }
+    case RESET: {
+      return 0;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default counterReducer;
