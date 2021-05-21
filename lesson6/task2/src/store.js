c6ca8e692a908794819e05e1a4aaf44de975bb73
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-21 17:07:51 +0300

    usersReducer

---
+++ lesson6/task2/src/store.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from "redux";
+import usersReducer from "./users.reducer";
+
+const appStore = combineReducers({
+  users: usersReducer,
+});
+
+const store = createStore(appStore, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
---
+++ lesson6/task2/src/users.reducer.js
@@ -0,0 +1,14 @@
+const initState = {
+  userData: null,
+  isFetching: false,
+};
+
+const usersReducer = (state = initState, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+export default usersReducer;
