5f5e30986a978594e374932c9001605a56ff93c1
IuriiKyrylovskyi <iurii712@ukr.net>
2021-05-21 17:39:03 +0300

    SearchField

---
+++ lesson6/task2/src/users/SearchField.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { connect } from "react-redux";
+import { userDataSelector } from "./users.selectors";
+
+const SearchField = ({ userData, handleChange }) => {
+  const { name } = userData;
+  return (
+    <div className="name-form">
+      <input
+        type="text"
+        className="name-form__input"
+        value={name}
+        onChange={handleChange}
+        //
+      />
+      <button
+        className="name-form__btn btn"
+        onClick={handleClick}
+        //
+      >
+        Show
+      </button>
+    </div>
+  );
+};
+
+const mapState = (state) => {
+  return {
+    userData: userDataSelector(state),
+  };
+};
+
+const mapDispatch = {
+  handleChange,
+};
+
+export default connect(mapState, mapDispatch)(SearchField);
